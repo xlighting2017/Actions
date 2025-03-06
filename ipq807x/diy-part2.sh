4fba569fc65ed76c52e2f6ad45ae8739960f5241
Rye Sears <xlighting@gmail.com>
2025-03-06 06:13:03 +0000

    first commit to support ipq807x

--- ipq807x/diy-part2.sh
+++ ipq807x/diy-part2.sh
@@ -11,10 +11,21 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# borrow some package from immortalwrt
+
+# shallow clone without actually pull all the files
+git clone --no-checkout --depth 1 https://github.com/immortalwrt/immortalwrt immortalwrt
+cd immortalwrt
+# sparse-checkout only certail paakages
+git sparse-checkout set "package/emortal/autocore" "package/emortal/cpufreq" "package/emortal/default-settings"
+git checkout
+# copy to package for further use
+cp -R package/emortal/* ../package/
